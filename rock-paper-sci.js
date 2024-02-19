4707cc63a61652940f6ecea59f60c6cc7c5acad3
eilleendwe <eilleenyeoh3@gmail.com>
2024-02-19 23:37:16 +0700

    almost finish

--- rock-paper-sci.js
+++ rock-paper-sci.js
@@ -7,11 +7,31 @@ function getCompChoice() {
 
 let playerScore = 0;
 let compScore = 0;
+let round = 0;
 
-function playRound(){
+const playerSign = document.getElementById('player-sign');
+const compSign = document.getElementById('comp-sign');
+
+function setDisplay(target, choice){
+    if(choice === "rock"){
+        target.textContent = "✊";
+    } else if (choice === "paper"){
+        target.textContent = "✋";
+    } else {
+        target.textContent = "✌";
+    }
+}
+
+let playerScores = document.getElementById('player-score');
+let compScores = document.getElementById('comp-score');
+
+
+function playRound(player){
     const comp = getCompChoice();
-    const player = prompt ("Choose rock, paper or scissors")
     
+    setDisplay(playerSign, player);
+    setDisplay(compSign, comp);
+
     // console.log(comp);
     // console.log(player);
 
@@ -21,35 +41,68 @@ function playRound(){
     const lose = (!draw && !win);
 
     if (draw) {
-        console.log(`Draw! Both chose ${player}`);
+        alert(`Draw! Both chose ${player}`);
     } else if (win) {
         playerScore ++;
-        console.log(`You win! ${player} beats ${comp}`);
+        alert(`You win! ${player} beats ${comp}`);
     } else {
         compScore ++;
-        console.log(`You lose! ${comp} beats ${player}`);
+        alert(`You lose! ${comp} beats ${player}`);
         
     }
+
+    playerScores.textContent = `Player: ${playerScore}`;
+    compScores.textContent = `Computer: ${compScore}`;
 
     console.log(`your score: ${playerScore}`);
     console.log(`computer score: ${compScore}`);
 
+    round ++;
 }
 
 
-function playGame(){
-    for (let i = 0; i < 5; i ++){
-        playRound();
-    }
+// function playGame(){
+//     for (let i = 0; i < 5; i ++){
+//         playRound();
+//     }
 
-    if (playerScore > compScore){
-        alert(`You win with ${playerScore} score`);
-    } else if (compScore > playerScore) {
-        alert(`Computer win with ${compScore} score`);       
-    } else {
-        alert(`Draw with ${compScore} score`)
-    }
+//     if (playerScore > compScore){
+//         alert(`You win with ${playerScore} score`);
+//     } else if (compScore > playerScore) {
+//         alert(`Computer win with ${compScore} score`);       
+//     } else {
+//         alert(`Draw with ${compScore} score`)
+//     }
+
+// }
+
+const btnList = document.querySelectorAll(".choice button");
+
+for (let i = 0; i < btnList.length; i++){
+    btnList[i].addEventListener("click", (e) => {
+        console.log(e.target.dataset.value);
+        const playerChoice = e.target.dataset.value;
+        playRound(playerChoice);
+        if (playerScore === 5 || compScore === 5){
+            if (playerScore === 5){
+                alert('You win');
+            }else {
+                alert('You lose!');
+            }
+
+            playerScore = 0;
+            compScore = 0;
+            playerScores.textContent = "Player: " + playerScore;
+            compScores.textContent = "Computer: " + compScore;
+
+
+        }
+    })
 
 }
 
-playGame();
+// rock.addEventListener("click", () =>{
+//     alert('123');
+// })
+
+//playGame();
